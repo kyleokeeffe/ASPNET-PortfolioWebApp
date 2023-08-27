4b46e20b43851b8f93929bdebe3e3983990d6f34
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2023-08-27 10:00:34 -0400

    Added page components/routing

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ProjectsComponent,
+    ProjectItemComponent,
+    AboutComponent
+
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -25,6 +33,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'about', component: AboutComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
