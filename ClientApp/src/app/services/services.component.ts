41efcb403f5199f8fce8e9076e4b84c2e1efd00f
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2023-08-28 15:12:52 -0400

    Debugged bootstrap carousel

--- ClientApp/src/app/services/services.component.ts
+++ ClientApp/src/app/services/services.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
-
+  webDevDiv: boolean = false;
+  standProgDiv: boolean = false;
+  dbAdminDiv: boolean = false;
+  resHostDiv: boolean = false;
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  ShowWebDev() {
+    this.webDevDiv = true;
+    this.standProgDiv = false;
+    this.dbAdminDiv = false;
+    this.resHostDiv = false;
+  }
+  ShowStandProg() {
+    this.webDevDiv = false;
+    this.standProgDiv = true;
+    this.dbAdminDiv = false;
+    this.resHostDiv = false;
+  }
+  ShowDbAdmin() {
+    this.webDevDiv = false;
+    this.standProgDiv = false;
+    this.dbAdminDiv = true;
+    this.resHostDiv = false;
+  }
+  ShowResHost() {
+    this.webDevDiv = false;
+    this.standProgDiv = false;
+    this.dbAdminDiv = false;
+    this.resHostDiv = true;
+  }
 }
